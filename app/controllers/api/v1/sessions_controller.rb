f5b8e57710d28280816705ccadc40ca09eb0ac28
Ruto Ken <ruto.k.ken@gmail.com>
2018-11-12 23:07:56 +0300

    cors ammended

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,38 @@
+    class Api::V1::SessionsController < Api::V1::ApiController
+        protect_from_forgery with: :null_session
+        acts_as_token_authentication_handler_for User, fallback: :none
+
+        def show
+          current_user ? head(:ok) : head(:unauthorized)
+        end
+
+       def create
+           if params[:email]
+             @user = User.where(email: params[:email]).first
+           else
+             @user = User.where(email: params[:email]).first if params[:username]
+           end
+
+           if @user&.valid_password?(params[:password])
+              render json: @user.as_json(only: [:id, :email, :authentication_token]), status: :created
+            else
+              head(:unauthorized)
+            end
+
+       end
+
+       def destroy
+          if nilify_token && current_user.save
+            head(:ok)
+          else
+            head(:unauthorized)
+          end
+       end
+
+       private
+
+       def nilify_token
+         current_user&.authentication_token = nil
+       end
+
+    end
